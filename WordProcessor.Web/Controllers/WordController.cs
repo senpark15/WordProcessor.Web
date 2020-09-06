4262789504671d81426be78713aa380206f63e2f
Senthil Palani <senthil@aveoninfotech.com>
2020-09-06 15:27:17 +0530

    Initial Commit

---
+++ WordProcessor.Web/Controllers/WordController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WordProcessor.Models;
+using WordProcessor.Service.Contracts;
+
+namespace WordProcessor.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+
+        private readonly ILogger<WordController> _logger;
+        private readonly IWordCountService wordCountService;
+
+        public WordController(ILogger<WordController> logger, IWordCountService wordCountService)
+        {
+            _logger = logger;
+            this.wordCountService = wordCountService;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(WordCountInput input)
+        {
+            return Ok(wordCountService.Process(input));
+        }
+    }
+}
---
+++ WordProcessor.Web/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WordProcessor.Service;
+using WordProcessor.Service.Contracts;
+
+namespace WordProcessor.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(o => o.AddPolicy("AllowAllPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            services.AddScoped<IWordCountService, WordCountService>();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Word Processor API V1");
+            });
+
+            app.UseCors("AllowAllPolicy");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
